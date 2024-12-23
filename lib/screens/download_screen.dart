4e5428ae6a62b8019b670f6ee1929113d7e526bf
pavan bagwe <96296445+pavanb0@users.noreply.github.com>
2024-12-23 17:35:48 +0530

    Update download_screen.dart

--- lib/screens/download_screen.dart
+++ lib/screens/download_screen.dart
@@ -15,34 +15,54 @@ class DownloadScreen extends StatefulWidget {
 
 class _DownloadScreenState extends State<DownloadScreen> {
   bool isloadingThumbnail = true;
+  bool isLoadingVideoUrl = true;
+  bool isLoadingAudioUrl = true;
+
   final yt = YoutubeExplode();
   late String thumbnail = "";
+  late String title = "";
+
+  late Map<Uri, String> videoUrlRes = Map();
+  late Map<String, String> videoQualitSize = Map();
+  //late
+  late Map<Uri, String> audioUrlsBitrate = Map();
+  late Map<String, String> audioBitrateSize = Map();
 
   void startdownload() async {
     try {
       var video = await yt.videos.get(widget.url);
-      print('Title: ${video.title}');
-      print('Thumbnail URL: ${video.thumbnails.highResUrl}');
+      //print('Title: ${video.title}');
+      //print('Thumbnail URL: ${video.thumbnails.highResUrl}');
       thumbnail = video.thumbnails.highResUrl;
       setState(() {
         isloadingThumbnail = false;
+        title = video.title;
       });
+
       var manifest = await yt.videos.streamsClient.getManifest(widget.url);
 
-      // Get all video formats
       var videoStreams = manifest.video;
       for (var stream in videoStreams) {
-        print(
-            'Video Stream: ${stream.url} | Resolution: ${stream.videoQuality}');
+        videoUrlRes[stream.url] = stream.videoResolution.toString();
+        videoQualitSize[stream.videoQuality.toString()] =
+            stream.size.toString();
       }
-
-      // Get all audio formats
+      setState(() {
+        isLoadingVideoUrl = false;
+      });
       var audioStreams = manifest.audioOnly;
       for (var stream in audioStreams) {
-        print(
-            'Audio Stream: ${stream.url} | Quality: ${stream.bitrate.kiloBitsPerSecond} kbps');
+        // print(
+        //     'Audio Stream: ${stream.url} | Quality: ${stream.bitrate.kiloBitsPerSecond} kbps');
+        audioUrlsBitrate[stream.url] =
+            stream.bitrate.kiloBitsPerSecond.toString();
+        audioBitrateSize[stream.bitrate.toString()] = stream.size.toString();
       }
+      setState(() {
+        isLoadingAudioUrl = false;
+      });
     } catch (C) {
+      Navigator.pop(context);
       rethrow;
     } finally {
       yt.close();
@@ -68,130 +88,127 @@ class _DownloadScreenState extends State<DownloadScreen> {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-          appBar: PreferredSize(
-            preferredSize: const Size.fromHeight(56),
-            child: OrientationBuilder(
-              builder: (context, orientation) {
-                Color startColor = (orientation == Orientation.portrait)
-                    ? const Color(0xFF1CFCF3)
-                    : const Color(0xFF74A9E6);
+        appBar: PreferredSize(
+          preferredSize: const Size.fromHeight(56),
+          child: OrientationBuilder(
+            builder: (context, orientation) {
+              Color startColor = (orientation == Orientation.portrait)
+                  ? const Color(0xFF1CFCF3)
+                  : const Color(0xFF74A9E6);
 
-                Color endColor = (orientation == Orientation.portrait)
-                    ? const Color(0xFF3FC1C9)
-                    : const Color(0xFF1CFCF3);
+              Color endColor = (orientation == Orientation.portrait)
+                  ? const Color(0xFF3FC1C9)
+                  : const Color(0xFF1CFCF3);
 
-                return AnimatedContainer(
-                  duration: const Duration(seconds: 1),
-                  curve: Curves.easeInOut,
-                  decoration: BoxDecoration(
-                    gradient: LinearGradient(
-                      colors: [startColor, endColor],
-                      begin: (orientation == Orientation.portrait)
-                          ? Alignment.topCenter
-                          : Alignment.bottomCenter,
-                      end: (orientation == Orientation.portrait)
-                          ? Alignment.bottomCenter
-                          : Alignment.topCenter,
+              return AnimatedContainer(
+                duration: const Duration(seconds: 1),
+                curve: Curves.easeInOut,
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    colors: [startColor, endColor],
+                    begin: (orientation == Orientation.portrait)
+                        ? Alignment.topCenter
+                        : Alignment.bottomCenter,
+                    end: (orientation == Orientation.portrait)
+                        ? Alignment.bottomCenter
+                        : Alignment.topCenter,
+                  ),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black.withOpacity(0.3),
+                      blurRadius: 10,
+                      spreadRadius: 3,
+                      offset: const Offset(0, 5),
                     ),
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.black.withOpacity(0.3),
-                        blurRadius: 10,
-                        spreadRadius: 3,
-                        offset: const Offset(0, 5),
+                  ],
+                ),
+                child: AppBar(
+                  title: const Row(
+                    children: [
+                      Text(
+                        "Dw Player",
+                        style: TextStyle(
+                          color: Color(0XFFF5F5F5),
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20,
+                        ),
+                      ),
+                      Spacer(),
+                      Icon(
+                        Icons.music_note_outlined,
+                        color: Color(0xfff5f5f5),
                       ),
                     ],
                   ),
-                  child: AppBar(
-                    title: const Row(
-                      children: [
-                        Text(
-                          "Dw Player",
-                          style: TextStyle(
-                            color: Color(0XFFF5F5F5),
-                            fontWeight: FontWeight.bold,
-                            fontSize: 20,
-                          ),
-                        ),
-                        Spacer(),
-                        Icon(
-                          Icons.music_note_outlined,
-                          color: Color(0xfff5f5f5),
-                        ),
-                      ],
-                    ),
-                    backgroundColor: Colors.transparent,
-                    elevation: 0,
-                  ),
-                );
-              },
-            ),
+                  backgroundColor: Colors.transparent,
+                  elevation: 0,
+                ),
+              );
+            },
           ),
-          body: SingleChildScrollView(
-            child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.all(18),
-                    child: Skeletonizer(
-                      enabled: isloadingThumbnail,
-                      enableSwitchAnimation: true,
-                      child: thumbnail.isNotEmpty
-                          ? ClipRRect(
-                              borderRadius: BorderRadius.circular(10),
-                              child: Image.network(
-                                thumbnail,
-                                fit: BoxFit.cover,
-                                width: double.infinity,
-                                height: 250,
-                              ),
-                            )
-                          : Shimmer.fromColors(
-                              baseColor: Color(0xff435364),
-                              highlightColor: Color(0xff101418),
-                              child: Container(
-                                width: double.infinity,
-                                height: 250,
-                                decoration: BoxDecoration(
-                                  color: Color(0xff101418),
-                                  borderRadius: BorderRadius.circular(10),
-                                ),
-                              ),
-                            ),
-                    ),
-                  ),
-                  // const SizedBox(
-                  //   height: 8,
-                  // ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 18),
-                    child: Skeletonizer(
-                      enabled: isloadingThumbnail,
-                      enableSwitchAnimation: true,
-                      child: Container(
-                        width: double.infinity,
-                        padding: const EdgeInsets.symmetric(
-                            vertical: 4, horizontal: 8),
-                        decoration: BoxDecoration(
-                          color: Colors.grey.shade900,
-                          borderRadius: BorderRadius.circular(5),
+        ),
+        body: SingleChildScrollView(
+          child:
+              Column(crossAxisAlignment: CrossAxisAlignment.center, children: [
+            Padding(
+              padding: const EdgeInsets.all(18),
+              child: Skeletonizer(
+                enabled: isloadingThumbnail,
+                enableSwitchAnimation: true,
+                child: thumbnail.isNotEmpty
+                    ? ClipRRect(
+                        borderRadius: BorderRadius.circular(10),
+                        child: Image.network(
+                          thumbnail,
+                          fit: BoxFit.cover,
+                          width: double.infinity,
+                          height: 250,
                         ),
-                        child: Text(
-                          isloadingThumbnail
-                              ? "Loading thumbnail..."
-                              : "Video Thumbnail",
-                          textAlign: TextAlign.center,
-                          style: const TextStyle(
-                            color: Colors.white,
-                            fontSize: 14,
+                      )
+                    : Shimmer.fromColors(
+                        baseColor: Color(0xff435364),
+                        highlightColor: Color(0xff101418),
+                        child: Container(
+                          width: double.infinity,
+                          height: 250,
+                          decoration: BoxDecoration(
+                            color: Color(0xff101418),
+                            borderRadius: BorderRadius.circular(10),
                           ),
                         ),
                       ),
+              ),
+            ),
+            // const SizedBox(
+            //   height: 8,
+            // ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 18),
+              child: Skeletonizer(
+                enabled: isloadingThumbnail,
+                enableSwitchAnimation: true,
+                child: Container(
+                  width: double.infinity,
+                  padding:
+                      const EdgeInsets.symmetric(vertical: 4, horizontal: 8),
+                  decoration: BoxDecoration(
+                    color: Colors.grey.shade900,
+                    borderRadius: BorderRadius.circular(5),
+                  ),
+                  child: Text(
+                    isloadingThumbnail ? "Loading thumbnail..." : title,
+                    textAlign: TextAlign.left,
+                    style: const TextStyle(
+                      color: Color(0xffffc600),
+                      fontSize: 18,
                     ),
                   ),
-
-                  Skeletonizer(
-                    enabled: isloadingThumbnail,
+                ),
+              ),
+            ),
+            isLoadingAudioUrl && isLoadingVideoUrl
+                ? Skeletonizer(
+                    enabled: true,
                     enableSwitchAnimation: true,
                     child: ListView.builder(
                       itemCount: 6,
@@ -210,9 +227,32 @@ class _DownloadScreenState extends State<DownloadScreen> {
                         );
                       },
                     ),
-                  ),
-                ]),
-          )),
+                  )
+                : ListView.builder(
+                    itemCount: videoUrlRes.length + audioUrlsBitrate.length,
+                    shrinkWrap: true,
+                    physics: NeverScrollableScrollPhysics(),
+                    padding: const EdgeInsets.all(16),
+                    itemBuilder: (context, index) {
+                      return Card(
+                        child: ListTile(
+                          title: videoUrlRes.length < index
+                              ? Text("Video Stream ${videoQualitSize}")
+                              : Text("Audio Stream"),
+                          subtitle: Text("Download in your desired format"),
+                          trailing: Icon(
+                            videoUrlRes.length < index
+                                ? Icons.video_collection_outlined
+                                : Icons.audio_file_outlined,
+                            size: 32,
+                          ),
+                        ),
+                      );
+                    },
+                  )
+          ]),
+        ),
+      ),
     );
   }
 }
